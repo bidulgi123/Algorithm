c260000f164818b406b2dbfc595823eb9d58db69
Kim Beom Su <121657338+bidulgi123@users.noreply.github.com>
2023-06-04 02:27:01 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a * 100 >= b)
+        cout << "Yes";
+    else
+        cout << "No";
+    return 0;
+}
