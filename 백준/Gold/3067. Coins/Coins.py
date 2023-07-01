6cb5dbbcbbdf548045b5f36b891f19fd8cf6fcb8
bidulgi123 <121657338+bidulgi123@users.noreply.github.com>
2023-07-01 18:21:54 +0900

    [Gold V] Title: Coins, Time: 176 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Gold/3067. Coins/Coins.py
@@ -0,0 +1,13 @@
+import sys
+from collections import deque
+for i in range(int(input())):
+    b=int(input())
+    c=list(map(int, input().split()))
+    e=int(input())
+    total=[0]*(e+1)
+    total[0]=1
+    for i in c :
+        for j in range(e+1):
+            if j >= i :
+                total[j] += total[j-i]
+    print(total[e])
