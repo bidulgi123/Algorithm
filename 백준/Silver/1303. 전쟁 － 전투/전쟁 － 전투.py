0b6e73b85f14d2a269e0088333ad32694c7b2585
bidulgi123 <121657338+bidulgi123@users.noreply.github.com>
2023-07-16 12:53:10 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 60 ms, Memory: 31268 KB -BaekjoonHub

---
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -0,0 +1,28 @@
+
+a,b=map(int,input().split())
+total=[list(input()) for i in range(b)]
+cnt=0
+p=[]
+q=[]
+def dfs(i,j,o):
+    global cnt
+    if i >= b or i < 0 or j >= a or j < 0 :
+        return False
+    if total[i][j] == o:
+        total[i][j] = '?'
+        dfs(i+1,j,o)
+        dfs(i-1,j,o)
+        dfs(i,j+1,o)
+        dfs(i,j-1,o)
+        cnt+=1
+        return True
+    return False
+for i in range(a) :
+    for j in range(b):
+        if dfs(j,i,'W')==True:
+            p.append(cnt**2)
+            cnt=0
+        if dfs(j,i,'B')==True:
+            q.append(cnt**2)
+            cnt=0
+print(sum(p),sum(q))
