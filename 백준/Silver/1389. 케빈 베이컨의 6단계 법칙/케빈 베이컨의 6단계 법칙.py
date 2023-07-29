dffecf4bbb662deff23a8f94c4cb3d5565ea93f4
bidulgi123 <121657338+bidulgi123@users.noreply.github.com>
2023-07-29 19:51:08 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 112 ms, Memory: 34680 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -8,10 +8,10 @@ for i in range(b):
  c,d=map(int,input().split())
  total[c][d] = 1
  total[d][c] = 1
-for k in range(a+1):
+for k in range(1,a+1):
     total[k][k] = 0
-    for i in range(a+1):
-        for j in range(a+1):
+    for i in range(1,a+1):
+        for j in range(1,a+1):
             total[i][j] = min(total[i][j], total[i][k] + total[k][j])
 ans=math.inf
 q=0
