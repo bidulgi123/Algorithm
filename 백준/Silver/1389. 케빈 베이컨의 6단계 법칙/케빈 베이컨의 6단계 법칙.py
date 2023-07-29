df68195b958c519b14bab0657e0f1581233b07d6
bidulgi123 <121657338+bidulgi123@users.noreply.github.com>
2023-07-29 19:34:27 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 124 ms, Memory: 34572 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+import math
+input=sys.stdin.readline
+a,b=map(int, input().split())
+total=[[math.inf]*(a+1) for i in range(a+1)]
+for i in range(b):
+ c,d=map(int,input().split())
+ total[c][d] = 1
+ total[d][c] = 1
+for k in range(a+1):
+    total[k][k] = 0
+    for i in range(a+1):
+        for j in range(a+1):
+            total[i][j] = min(total[i][j], total[i][k] + total[k][j])
+ans=math.inf
+q=0
+for i in range(1,len(total)):
+   total[i] = deque(total[i])
+   total[i].popleft()
+   if ans > sum(total[i]):
+      ans = sum(total[i])
+      q=i
+print(q)
