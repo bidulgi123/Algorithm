da1af9a4d90e7be07594aa81e65ef9864e3e8320
Kim Beom Su <121657338+bidulgi123@users.noreply.github.com>
2023-10-15 14:39:45 +0900

    [Silver II] Title: 양 한마리... 양 두마리..., Time: 268 ms, Memory: 32684 KB -BaekjoonHub

--- 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.py
+++ 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.py
@@ -2,14 +2,14 @@ import sys
 input=sys.stdin.readline
 sys.setrecursionlimit(1987654321)
 move = [(0, 1),(0, -1),(1, 0),(-1, 0)]
-def bfs(y,x):
+def dfs(y,x):
     if y >= b or y < 0 or x >= c or x < 0 :
         return False
     if total[y][x] == '#':
         total[y][x] = '?'
         for dy, dx in move:
             ny, nx = y + dy, x + dx,
-            bfs(ny,nx)
+            dfs(ny,nx)
         return True
     return False
 a=int(input())
@@ -19,6 +19,6 @@ for i in range(a):
     total=[list(input().rstrip())for i in range(b)]
     for i in range(b):
         for j in range(c):
-            if bfs(i,j) == True:
+            if dfs(i,j) == True:
                 cnt+=1
     print(cnt)
