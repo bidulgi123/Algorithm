e25a066764c6e2317b57d86fe852fdfb5c28c01d
Kim Beom Su <121657338+bidulgi123@users.noreply.github.com>
2023-10-19 00:20:58 +0900

    [Gold V] Title: 평범한 배낭, Time: 5052 ms, Memory: 39188 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,17 +1,19 @@
 import sys
 from collections import deque
-input=sys.stdin.readline
-a,b=map(int, input().split())
-mul=deque()
+input = sys.stdin.readline
+a, b = map(int, input().split())
+mul = deque()
 for i in range(a):
-    c=list(map(int,input().split()))
+    c = list(map(int, input().split()))
     mul.append(c)
-mul.appendleft([0,0])
-total=[[0 for i in range(b+1)] for i in range(a+1)]
-for i in range(1,a+1):
-    for j in range(1, b+1):
-        if j < mul[i][0] :
-            total[i][j]=total[i-1][j]
-        else : 
-            total[i][j]=max(mul[i][1]+total[i-1][j-mul[i][0]],total[i-1][j])
-print(total[a][b])
+mul.appendleft([0, 0])
+total = [0] * (b + 1)
+for i in range(1, a + 1):
+    new_total = [0] * (b + 1)
+    for j in range(1, b + 1):
+        if j < mul[i][0]:
+            new_total[j] = total[j]
+        else:
+            new_total[j] = max(mul[i][1] + total[j - mul[i][0]], total[j])
+    total = new_total
+print(total[b])
