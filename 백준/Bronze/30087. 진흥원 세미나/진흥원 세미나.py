955c5bd4144716f9f8b3267c50051c6cf1bbc538
bidulgi123 <121657338+bidulgi123@users.noreply.github.com>
2023-12-26 23:53:56 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 68 ms, Memory: 34008 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,16 @@
+import sys
+from collections import deque
+input=sys.stdin.readline
+a=int(input())
+total={'Algorithm':	204,
+'DataAnalysis':	207,
+'ArtificialIntelligence':	302,
+'CyberSecurity':	'B101',
+'Network':	303,
+'Startup':	501,
+'TestStrategy':	105}
+for i in range(a):
+    print(total[input().rstrip()])
+
+
+
