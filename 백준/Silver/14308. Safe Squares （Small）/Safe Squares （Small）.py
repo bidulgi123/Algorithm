76338e2c7a79114b876386a2c30b07b1b9f1eeae
bidulgi123 <121657338+bidulgi123@users.noreply.github.com>
2024-06-18 19:31:22 +0900

    [Silver V] Title: Safe Squares (Small), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14308. Safe Squares （Small）/Safe Squares （Small）.py
+++ 백준/Silver/14308. Safe Squares （Small）/Safe Squares （Small）.py
@@ -1,20 +1,27 @@
 import sys
 input = sys.stdin.readline
 a = int(input())
-for __ in range(1,a+1):
+for __ in range(1, a+1):
     x, y, t = map(int, input().split())
-    total = [[0]*y for i in range(x)]
-    cnt = x * y - t
+    matrix = [[0] * y for _ in range(x)]
+    prefix_sum = [[0] * (y+1) for _ in range(x+1)]
     for _ in range(t):
-        j, k =map(int, input().split())
-        total[j][k] = 1
-    search = min(x, y)
-    for i in range(2, search+1):
-        for n in range(y-i+1):
-            for u in range(x-i+1):
-                q = total[u][n:n+i]
-                for pp in range(i):
-                    q += total[u+pp][n:n+i]
-                if 1 not in q :
-                    cnt+=1
-    print(f"Case #{__}:",cnt)
+        j, k = map(int, input().split())
+        matrix[j][k] = 1
+    for i in range(x):
+        for j in range(y):
+            prefix_sum[i+1][j+1] = matrix[i][j] + prefix_sum[i+1][j] + prefix_sum[i][j+1] - prefix_sum[i][j]
+    cnt = 0
+    for i in range(1, x+1):
+        for j in range(1, y+1):
+            max_size = min(x - i + 1, y - j + 1)
+            for size in range(1, max_size + 1):
+                total_sum = (prefix_sum[i+size-1][j+size-1]
+                                - prefix_sum[i+size-1][j-1]
+                                - prefix_sum[i-1][j+size-1]
+                                + prefix_sum[i-1][j-1])
+                if total_sum == 0:
+                    cnt += 1
+                else:
+                    break
+    print(f"Case #{__}: {cnt}")
