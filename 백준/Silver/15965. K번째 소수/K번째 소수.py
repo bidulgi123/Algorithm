e7e324e81ccf2ea2ea816c275ce074ecb939a3a5
bidulgi123 <121657338+bidulgi123@users.noreply.github.com>
2024-09-30 02:58:40 +0900

    [Silver II] Title: K번째 소수, Time: 288 ms, Memory: 40736 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/15965. K번째 소수/K번째 소수.py
@@ -0,0 +1,12 @@
+a = int(input())
+p = 1000000 
+t = [1 for _ in range(p)]  
+s = []  
+for i in range(2, p):
+    if t[i] == 1:  
+        s.append(i) 
+        for j in range(i * i, p, i):  
+            t[j] = 0 
+    if len(s) == a: 
+        break
+print(s[-1])
