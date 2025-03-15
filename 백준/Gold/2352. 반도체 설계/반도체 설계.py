9d9603af8edf2291285160e3c696fe94acba5835
bidulgi123 <121657338+bidulgi123@users.noreply.github.com>
2025-03-15 16:14:32 +0900

    [Gold II] Title: 반도체 설계, Time: 56 ms, Memory: 37892 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.py
@@ -0,0 +1,11 @@
+from bisect import bisect_left
+a=int(input())
+b=list(map(int, input().split()))
+total=[b[0]]
+for i in b:
+    if total[-1] < i:
+        total.append(i)
+    else : 
+        p=bisect_left(total, i)
+        total[p]=i
+print(len(total))
